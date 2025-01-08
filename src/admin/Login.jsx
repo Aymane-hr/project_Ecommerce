79422a2971039ff549a738e7d250aa890e39f3c1
Aymane hr <aymanhar20@gmail.com>
2025-01-08 21:53:07 +0100

    commit of 8

--- src/admin/Login.jsx
+++ src/admin/Login.jsx
@@ -14,17 +14,19 @@ const Login = () => {
   const { login } = useAuth();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => { 
     e.preventDefault();
+    setError(""); // Clear any previous errors
 
     try {
       await login(username, password); 
       navigate("/admin"); 
     } catch (error) {
       console.error("Login failed:", error); 
-      // Handle login errors (e.g., display an error message)
+      setError("Login failed. Please check your username and password."); // Set error message
     }
   };
 
@@ -36,37 +38,33 @@ const Login = () => {
           <img
             src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
             className="img-fluid"
-            alt="Phone illustration"
+            alt="Phone image"
           />
         </MDBCol>
 
         {/* Right Form Column */}
         <MDBCol col="4" md="6">
           <form onSubmit={handleSubmit}>
-            <h1 className="mb-4">Admin Login</h1>
-
             <MDBInput
               wrapperClass="mb-4"
               label="Username"
-              id="username"
+              id="formControlLg"
               type="text"
+              size="lg"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              required
-              size="lg"
             />
             <MDBInput
               wrapperClass="mb-4"
               label="Password"
-              id="password"
+              id="formControlLg"
               type="password"
+              size="lg"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              required
-              size="lg"
             />
-
-            <MDBBtn type="submit" className="mb-4 w-100" size="lg">
+            {error && <div className="error-message">{error}</div>}
+            <MDBBtn className="mb-4 w-100" size="lg" type="submit">
               Sign in
             </MDBBtn>
           </form>
